cbfd59e5b1f1439ea164ffecb2ec70acdbb5ab37
Component Creator (Jensen Technologies S.L) <info@component-creator.com>
2014-03-19 11:09:15 +0100

    Testing

--- text.php
+++ text.php
@@ -1,44 +1,156 @@
 <?php
 
-// No direct access
-defined('_JEXEC') or die('Restricted access');
+//Including the API PHP library
+require_once JPATH_COMPONENT . '/assets/php/vendor/autoload.php';
 
-//sessions
-jimport('joomla.session.session');
+class GithubHelper {
 
-require_once JPATH_COMPONENT_ADMINISTRATOR . '/helpers/combuilder.php';
-require_once JPATH_COMPONENT . '/helpers/github.php';
+    /**
+     * Method for getting the Github access token configured by the user.
+     * @return mixed returns the access token if it exists, null otherwise
+     */
+    private static function getAccessToken($jcc_token) {
 
-GithubHelper::createFile('WhatsAppND', 'Testing API', __FILE__);
+        if ($jcc_token) {
+            jimport('joomla.application.component.helper');
+            return JComponentHelper::getParams('com_combuilder')->get('github_account_token_jcc');
+        } else {
+            $dispatcher = JDispatcher::getInstance();
+            JPluginHelper::importPlugin('user');
+            $user = JFactory::getUser();
+            $dispatcher->trigger('onContentPrepareData', array('com_users.profile', $user));
 
-CombuilderHelper::updateUserInSession();
-CombuilderHelper::setItemids(); //Set the items ids of the menus ('Itemid' param)
-//Load classes
-JLoader::registerPrefix('Combuilder', JPATH_COMPONENT);
+            return empty($user->nwd['github_access_token']) ? null : $user->nwd['github_access_token'];
+        }
+    }
 
-//Load plugins
-JPluginHelper::importPlugin('Combuilder');
+    /**
+     * Loads all the repository from a particular user.
+     * @param $username The username of the repositories owner. If $username contains null, gets all the repositories from the current logged user.
+     * @return array An array with all the repositories information.
+     */
+    public static function getRepositories($username = null) {
+        //Create a github client
+        $github = self::createClientAuthenticate();
 
-//application
-$app = JFactory::getApplication();
+        //If the username was not specified, let's load the repositories from the current logged user.
+        if (empty($username)) {
+            $repos = $github->api('current_user')->repositories();
+        } else {
+            $repos = $github->api('current_user')->repositories();
+        }
 
-//To prevent rewriting a lot of controller code we are here mimicking the behaviour from Joomla 2.5
-// task=controller.method
-$task = $app->input->get('task', '');
-if (!empty($task) && count(explode('.', $task)) > 1) {
-    $task_data = explode('.', $task);
+        return $repos;
+    }
 
-    $controller = $task_data[0];
-    $method = $task_data[1];
-} else {
-    // Require specific controller if requested
-    $controller = $app->input->get('controller', 'default');
-    $method = 'execute';
+    /**
+     * Get all the pull requests from a repository
+     * @param type $repository
+     * @param type $username
+     * @return type
+     */
+    public static function getPullRequests($repository, $username = null) {
+        //Create a github client
+        $github = self::createClientAuthenticate();
+
+        //If the username is null, let's get the current logged in user username
+        if (empty($username)) {
+            $username = self::getUsernameOfCurrentUserLogged($github);
+        }
+        return $github->api('pull_request')->all($username, $repository);
+    }
+
+    public static function createFile($repository, $commit_message, $path_file, $branch = null) {
+        //Create a github client
+        $github = self::createClientAuthenticate();
+
+        //Getting the username of the current logged user
+        $username = self::getUsernameOfCurrentUserLogged($github);
+
+        //Getting the contents of the file
+        $file_content = file_get_contents($path_file);
+        $response = $github->api('repo')->contents()->create($username, $repository, 'text.php', $file_content, $commit_message, $branch);
+        echo '<pre class="debug"><small>' . __file__ . ':' . __line__ . "</small>\n\$response = " . print_r($response, true) . "\n</pre>";
+        exit;
+    }
+
+    public static function updateFile($repository, $commit_message, $path_file, $jcc_user = false, $branch = null) {
+        //Create a github client
+        $github = self::createClientAuthenticate($jcc_user);
+
+        //Getting the username of the current logged user
+        $username = self::getUsernameOfCurrentUserLogged($github);
+
+        //Getting the contents of the file
+        $file_content = file_get_contents(__FILE__);
+        $file_info = self::getFileInformation($repository, 'text.php', $jcc_user, $branch);
+        $response = $github->api('repo')->contents()->update($username, $repository, 'text.php', $file_content, $commit_message, $file_info['sha'], $branch);
+        echo '<pre class="debug"><small>' . __file__ . ':' . __line__ . "</small>\n\$response = " . print_r($response, true) . "\n</pre>";
+        exit;
+    }
+
+    public static function getFileInformation($repository, $path_file, $jcc_user = false, $branch = null) {
+        //Create a github client
+        $github = self::createClientAuthenticate($jcc_user);
+
+        //Getting the username of the current logged user
+        $username = self::getUsernameOfCurrentUserLogged($github);
+
+        //Getting the contents of the file
+        $response = $github->api('repo')->contents()->show($username, $repository, $path_file, $branch);
+
+        return $response;
+    }
+
+    public static function isFileCommited($repository, $path_file, $branch = null) {
+        //Create a github client
+        $github = self::createClientAuthenticate();
+
+        //Getting the username of the current logged user
+        $username = self::getUsernameOfCurrentUserLogged($github);
+
+        //Getting the contents of the file
+        $response = $github->api('repo')->contents()->show($username, $repository);
+
+        echo '<pre class="debug"><small>' . __file__ . ':' . __line__ . "</small>\n\$response = " . print_r($response, true) . "\n</pre>";
+        exit;
+    }
+
+    /**
+     * Create a fork from a repository
+     * @param string owner username
+     * @param string $repository Repository name
+     * @param boolean $jcc_user
+     * @return array New repository data.
+     */
+    public static function forkRepository($username, $repository, $jcc_user) {
+        //Create a github client
+        $github = self::createClientAuthenticate($jcc_user);
+
+        //Forking the repository
+        $repository = $github->api('repo')->forks()->create($username, $repository);
+        return $repository;
+    }
+
+    /**
+     * Create a Github client authenticate by the OAuth token
+     * @return \Github\Client Github client object
+     */
+    private static function createClientAuthenticate($jcc_account) {
+        $github = new \Github\Client();
+        $github->authenticate(self:: getAccessToken($jcc_account), Github\Client::AUTH_HTTP_TOKEN);
+
+        return $github;
+    }
+
+    /**
+     * Get the username of the current user logged.
+     * @param \Github\Client $github Github client object
+     * @return string The username
+     */
+    private static function getUsernameOfCurrentUserLogged($github) {
+        $user_data = $github->api('current_user')->show();
+        return $user_data['login'];
+    }
+
 }
-
-// Create the controller
-$classname = 'CombuilderControllers' . ucwords($controller);
-$controller = new $classname();
-
-// Perform the Request task
-$controller->$method();
